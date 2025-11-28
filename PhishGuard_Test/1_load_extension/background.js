1a3f23e50eea7b70e8f487f98ecd566d238655b4
Michael Koppel <mkoppel6@gatech.edu>
2025-11-28 12:48:18 -0800

    Rename

--- PhishGuard_Test/1_load_extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "analyze") {
-      let url = message.url;
-      // TODO: Call ML model service
-      sendResponse({result: "Safe ✅ (placeholder)"});
-    }
-  });
-  
--- PhishGuard_Test/1_load_extension/content.js
+++
@@ -1,6 +0,0 @@
-// Example: redirect if page flagged as phishing
-if (window.location.href.includes("suspicious-example.com")) {
-    alert("⚠️ This site may be a phishing attempt!");
-    window.stop();
-  }
-  
